1d3b54b8f2139aad52a4abe96ff9da6dc0a32521
KubraKurdoglu <kubrakurdoglusimsek@gmail.com>
2023-09-09 23:55:01 +0200

    09 sept

---
+++ src/test/java/put2/Put03.java
@@ -0,0 +1,57 @@
+package put2;
+
+import base_urls.DummyRestApiBaseUrl;
+import base_urls.JsonPlaceHolderBaseUrl;
+import io.restassured.response.Response;
+import org.junit.Test;
+import pojos.JsonPlaceHolderPojo;
+import test_data.JsonPlaceHolderTestData;
+import utils.ObjectMapperUtils;
+
+import java.util.HashMap;
+
+import static io.restassured.RestAssured.given;
+
+public class Put03 extends JsonPlaceHolderBaseUrl {
+
+    /*
+    Given
+        https://jsonplaceholder.typicode.com/todos/198
+    When
+         I send GET Request to the URL
+     Then
+         Status code is 200
+         And response body is like {
+                                    "userId": 10,
+                                    "id": 198,
+                                    "title": "quis eius est sint explicabo",
+                                    "completed": true
+                                  }
+
+*/
+
+    @Test
+    public void test01(){
+
+        //set the url
+        spec.pathParams("first", "todos", "second", 198);
+
+        //set the expected data
+
+
+        //send the request and get the response
+        Response response = given(spec).put("{first}/{second}");
+        response.prettyPrint();
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
