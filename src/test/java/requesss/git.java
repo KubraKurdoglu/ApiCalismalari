f0d3df905f301341ec02fce3355463a1ccce9d8d
KubraKurdoglu <kubrakurdoglusimsek@gmail.com>
2023-09-21 23:23:17 +0200

    21 sept

--- src/test/java/requesss/git.java
+++ src/test/java/requesss/git.java
@@ -3,8 +3,11 @@ package requesss;
 import base_urls.JsonPlaceHolderBaseUrl;
 import io.restassured.response.Response;
 import org.junit.Test;
+import pojos.GoRestDataPojo;
+import pojos.GoRestPojo;
 
 import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
 
 public class git extends JsonPlaceHolderBaseUrl {
 
@@ -22,11 +25,31 @@ public class git extends JsonPlaceHolderBaseUrl {
 
         //send the request and the response
 
+        //set the expected data
+        GoRestDataPojo goRestDataPojo =  new GoRestDataPojo("Ganapati Prajapat","prajapat_ganapati@okeefe.org",
+                "female", "active");
+        GoRestPojo expectedData =  new GoRestPojo(null, goRestDataPojo);
+        System.out.println("expectedData = " + expectedData);
+
+        //Send the request and get the rsponse
+
+        //Do assertion
+        GoRestPojo actualData = response.as(GoRestPojo.class);
+        System.out.println("actualData = " + actualData);
+
+        assertEquals(200, response.statusCode());
+        assertEquals(expectedData.getMeta(), actualData.getMeta());
+        assertEquals(goRestDataPojo.getName(), actualData.getData().getName());
+        assertEquals(goRestDataPojo.getEmail(), actualData.getData().getEmail());
+        assertEquals(goRestDataPojo.getGender(), actualData.getData().getGender());
+        assertEquals(goRestDataPojo.getStatus(), actualData.getData().getStatus());
 
 
+        //assertEquals(expectedData.getData().getName(), actualData.getData().getName());
+        //expectedData.getData()==> bunun yerine, yani bu kadar uzatmak yerine, olusturulan pojo class adini direk
+        //yazabilirsin
 
-
-
+        //bir de burda "setter" lari kullanmadik, ama data degistirilmesi gereken yerde kullanilabilir
 
 
     }
