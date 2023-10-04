073a358f6ecc9dff304c7fede3c1d27106278ca8
KubraKurdoglu <kubrakurdoglusimsek@gmail.com>
2023-10-04 23:38:15 +0200

    04 OCT

---
+++ src/test/java/requesss/C01.java
@@ -0,0 +1,30 @@
+package requesss;
+
+import base_urls.HerOkuAppBaseUrl;
+import io.restassured.response.Response;
+import org.junit.Test;
+import pojos.BookingDatesPojo;
+import pojos.BookingPojo;
+
+import static herokuapp_smoketest.C01_PostRequest.bookingId;
+import static io.restassured.RestAssured.given;
+
+public class C01 extends HerOkuAppBaseUrl {
+
+    @Test
+    public void test01(){
+        //set the url
+        spec.pathParams("first", "booking", "second", bookingId);
+
+        //set the expected data
+        BookingDatesPojo bookingDatesPojo = new BookingDatesPojo("2018-01-01", "2019-01-01");
+        BookingPojo expectedData = new BookingPojo("Ali", "Can Can", 222, true, bookingDatesPojo, "BreakFeast");
+        System.out.println("expectedData = " + expectedData);
+
+        //send the request and get the response
+        Response response = given(spec).body(expectedData).put("{first}/{second}");
+        response.prettyPrint();
+    }
+
+
+}
