47e89db0c607487133ca8628ae5e1d6104550c6a
KubraKurdoglu <kubrakurdoglusimsek@gmail.com>
2023-10-12 22:15:45 +0200

    12 oct

---
+++ src/test/java/requesss/C07.java
@@ -0,0 +1,41 @@
+package requesss;
+
+import base_urls.ReqresBaseUrl;
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class C07 extends ReqresBaseUrl {
+    /*
+        Given
+            https://reqres.in/api/users/3
+        When
+            User sends a GET Request to the url
+        Then
+            HTTP Status Code should be 200
+        And
+            Content Type should be "application/json; charset=utf-8"
+        And
+            Status Line should be HTTP/1.1 200 OK
+     */
+
+
+    @Test
+    public void odev01(){
+
+        // i)Set the url
+        spec.pathParams("first", "users", "second", 3);
+
+
+        //ii) Set the expected data
+
+        // iii) Send the request and get the response
+        Response response = given(spec).get("{first}/{second}");
+        response.prettyPrint();
+
+        //iv) Do assertion --> Doğrulama yap
+        response.then().statusCode(200).contentType("application/json; charset=utf-8").statusLine("HTTP/1.1 200 OK");
+
+    }
+}
